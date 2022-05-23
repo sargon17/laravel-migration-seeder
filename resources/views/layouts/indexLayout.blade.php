0c044e791bf8000d0442ed65617e90b93dd55bb7
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-05-24 00:23:46 +0200

    Update indexLayout.blade.php

--- resources/views/layouts/indexLayout.blade.php
+++ resources/views/layouts/indexLayout.blade.php
@@ -1,3 +1,49 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/app.css">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <table>
+            <tr>
+                <th>
+                    ID
+                </th>
+                <th>
+                    company name
+                </th>
+                <th>
+                    duration
+                </th>
+                <th>
+                    depart time
+                </th>
+                <th>
+                    arrive time
+                </th>
+                <th>
+                    depart station
+                </th>
+                <th>
+                    arrive station
+                </th>
+                <th>
+                    status
+                </th>
+                <th>
+                    delay
+                </th>
+            </tr>
+            @yield('trains_info')
+        </table>
 
-?>
+    </div>
+
+
+</body>
+</html>
