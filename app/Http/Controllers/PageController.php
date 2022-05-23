3fb3bad7f76a91c8c9810010a0c41845aa545c62
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-05-23 23:20:10 +0200

    Update PageController.php

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,11 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Train;
 
 class PageController extends Controller
 {
     public function index()
     {
-        return view("index");
+        $trains = Train::select(
+            "trains.id",
+            "trains.train_code",
+            "SA.name as arrive_station",
+            "SD.name as depart_station",
+            "trains.arrive_time",
+            "trains.depart_time",
+            "state",
+            "delay"
+        )
+            ->join("companies as C", "trains.company_id", "=", "C.id")
+            ->join("stations as SA", "trains.arrive_station_id", "=", "SA.id")
+            ->join("stations as SD", "trains.depart_station_id", "=", "SD.id")
+            ->get();
+
+        return view("index", compact("trains"));
     }
 }
