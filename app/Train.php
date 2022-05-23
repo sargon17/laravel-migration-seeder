6c06f9f7b8d0c68ed326d234db36e4275e989073
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-05-24 00:23:06 +0200

    added methods to train model

--- app/Train.php
+++ app/Train.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Train extends Model
 {
-    //
+    public function fromDateTime($value)
+    {
+        return date("H:i d-m-Y", strtotime($value));
+    }
+
+    public function dateDifference()
+    {
+        $date1 = new \DateTime($this->depart_time);
+        $date2 = new \DateTime($this->arrive_time);
+        $diff = $date1->diff($date2);
+
+        return $diff->format("%H:%I");
+    }
 }
